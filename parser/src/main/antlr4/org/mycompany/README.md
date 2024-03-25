f88c02ee07102b02c14833e7412d1004a8ee38fe
Patrick Sullivan <patricksullivan@fb.com>
2024-03-24 23:42:28 -0700

    start of sql parsing

---
+++ parser/src/main/antlr4/org/mycompany/README.md
@@ -0,0 +1,3 @@
+- SqlBase is a copy of Presto's grammar file. It was not apparent to me how to initialize all the available grammars
+- SqlGrammarCopy is a direct copy from https://medium.com/@sasidharc/build-sql-parser-using-antlr4-part1-2044916a8406
+- SqlGrammar is the above, but edited. Use this as a sandbox.
--- parser/src/main/java.org.mycompany/Main.java
+++ parser/src/main/java.org.mycompany/Main.java
@@ -6,19 +6,29 @@ import org.mycompany.*;
 public class Main {
   public static void main(String[] args) throws IOException {
 
+    // This works in lexer/parser, but not lexer/parser{2} (presto grammar). Cookie for who
+    // figures out why.
+    String inputString = "SELECT col1, col2 FROM tbl;";
+
     // create a CharStream that reads from standard input
-    ANTLRInputStream input = new ANTLRInputStream(System.in);
+    ANTLRInputStream input = new ANTLRInputStream(inputString);
 
         // create a lexer that feeds off of input CharStream
-        ArrayInitLexer lexer = new ArrayInitLexer(input);
+    SqlGrammarLexer lexer = new SqlGrammarLexer(input);
+    SqlBaseLexer lexer2 = new SqlBaseLexer(input);
 
-        // create a buffer of tokens pulled from the lexer
+    // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+    CommonTokenStream tokens2 = new CommonTokenStream(lexer2);
 
-        // create a parser that feeds off the tokens buffer
-        ArrayInitParser parser = new ArrayInitParser(tokens);
+    // create a parser that feeds off the tokens buffer
+    SqlGrammarParser parser = new SqlGrammarParser(tokens);
+    SqlBaseParser parser2 = new SqlBaseParser(tokens);
 
-        ParseTree tree = parser.init(); // begin parsing at init rule
+    // Play around in the .g4 files to understand how myGrammar is generated!
+    ParseTree tree = parser.myGrammar();
+    ParseTree tree2 = parser2.singleStatement();
+
         System.out.println(tree.toStringTree(parser)); // print LISP-style tree
 
   }
